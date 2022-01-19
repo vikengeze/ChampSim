14bad795ae20e0635e19c22b37e0c623a14d94dd
vikengzekelian <vikengzekelian@hotmail.com>
2022-01-19 15:05:59 +0200

    tracing works

--- obj/prefetcher/stlb_prefetcher.d
+++ obj/prefetcher/stlb_prefetcher.d
@@ -1,8 +1,6 @@
 obj/prefetcher/stlb_prefetcher.o: prefetcher/stlb_prefetcher.cc \
- inc/ooo_cpu.h inc/cache.h inc/memory_class.h inc/champsim.h inc/block.h \
- inc/instruction.h inc/set.h inc/cache.h
-
-inc/ooo_cpu.h:
+ inc/cache.h inc/memory_class.h inc/champsim.h inc/block.h \
+ inc/instruction.h inc/set.h
 
 inc/cache.h:
 
@@ -15,5 +13,3 @@ inc/block.h:
 inc/instruction.h:
 
 inc/set.h:
-
-inc/cache.h:
